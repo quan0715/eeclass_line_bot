7b17b70fafa55a9a4f186dfa692548adffac338f
AlbertDoggyLin <albertlin2468@gmail.com>
2023-10-07 20:41:14 +0800

    refactor eeclass related functions and delete factored content

--- eeclass_line_bot/chatBotModel.py
+++ eeclass_line_bot/chatBotModel.py
@@ -1,14 +1,6 @@
-import json
 from .chatBotExtension import chat_status, jump_to, text, button_group
-import aiohttp
-from NotionBot.NotionClient import Notion
-from typing import Dict
+from notion_auth.models import LineUser
 
-import aiohttp
-from NotionBot import *
-from NotionBot.base.Database import *
-from NotionBot.object import *
-from NotionBot.object.BlockObject import *
 
 @chat_status("default")
 @button_group('EECLASS HELPER', '輸入以下指令開啟下一步', '輸入以下指令開啟下一步')
@@ -55,7 +47,6 @@ def oauth_connection(event):
     return message
     
 
-from notion_auth.models import LineUser
 @chat_status("set eeclass account")
 @text
 def set_eeclass_account(event):
@@ -78,8 +69,8 @@ def set_eeclass_password(event):
     jump_to(default_message, event.source.user_id, True)
     return f'已更新你的密碼為 {event.message.text}'
 
-from eeclass_bot.EEAsyncBot import EEAsyncBot
 import asyncio
+from .eeclass import eeclass_test_login
 @chat_status("test eeclass login")
 @text
 def test_eeclass_login(event):
@@ -91,7 +82,7 @@ def test_eeclass_login(event):
         loop=asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
     try:
-        task = loop.create_task(eeclass_test(user.eeclass_username, user.eeclass_password, user))
+        task = loop.create_task(eeclass_test_login(user.eeclass_username, user.eeclass_password))
         loop.run_until_complete(task)
         login_success = task.result()
     except Exception as e:
@@ -102,151 +93,3 @@ def test_eeclass_login(event):
     else:
         return '帳號認證失敗，請重新設定帳號密碼'
     
-
-async def eeclass_test(account, password, user: LineUser):
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-        bot = EEAsyncBot(session, account, password)
-        if not await bot.login(): return False
-        await bot.retrieve_all_course(check=True, refresh=True)
-        await bot.retrieve_all_bulletins()
-        all_bulletins_detail = await bot.retrieve_all_bulletins_details()
-        await bot.retrieve_all_homeworks()
-        all_homework_detail = await bot.retrieve_all_homeworks_details()
-        await bot.retrieve_all_material()
-        all_material_detail = await bot.retrieve_all_materials_details()
-        notion_bot = Notion(user.notion_token)
-        db = notion_bot.get_database(user.eeclass_db_id)
-        await update_all_bulletin_info_to_notion_db(bot.bulletins_detail_list , db)
-        await update_all_homework_info_to_notion_db(bot.homeworks_detail_list, db)
-        await update_all_material_info_to_notion_db(bot.materials_detail_list, db)
-
-def builtin_in_notion_template(db: Database, target):
-    return BaseObject(
-        parent=Parent(db),
-        properties=Properties(
-            Title=TitleValue(target['title']),
-            Course=SelectValue(target['course']),
-            ID=TextValue(target['ID']),
-            Announce_Date=DateValue(NotionDate(**target['date'])),
-            link=UrlValue(target['url']),
-            label=SelectValue("公告")
-        ),
-        children=Children(
-            CallOutBlock(f"發佈人 {target['發佈人']}  人氣 {target['人氣']}", color=Colors.Background.green),
-            QuoteBlock(f"內容"),
-            ParagraphBlock(target['content']["公告內容"]),
-            ParagraphBlock(" "),
-            QuoteBlock(f"連結"),
-            *[ParagraphBlock(TextBlock(content=links['名稱'], link=links['連結'])) for links in
-              target['content']['連結']],
-            ParagraphBlock(" "),
-            QuoteBlock(f"附件"),
-            *[ParagraphBlock(TextBlock(content=links['名稱'], link=links['連結'])) for links in
-              target['content']['附件']],
-        ),
-        icon=Emoji("🐶"),
-    )
-
-
-def homework_in_notion_template(db: Database, target):
-    cover_file_url = "https://images.pexels.com/photos/13010695/pexels-photo-13010695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    children_list = []
-    for key, value in target['content'].items():
-        children_list.append(QuoteBlock(TextBlock(key.capitalize()), color=Colors.Text.red))
-        if key == 'attach' or key == 'link':
-            children_list.extend([
-                BulletedBlock(TextBlock(content=a['title'], link=a['link'])) for a in value
-            ])
-        else:
-            result = TextBlock.check_length_and_split(value)
-            text = [TextBlock(r) for r in result] if result else [TextBlock(value)]
-            children_list.append(ParagraphBlock(*text)),
-
-        children_list.append(DividerBlock()),
-
-    return BaseObject(
-        parent=Parent(db),
-        properties=Properties(
-            Title=TitleValue(target['title']),
-            Course=SelectValue(target['course']),
-            ID=TextValue(target['ID']),
-            Deadline=DateValue(NotionDate(**target['date'])),
-            link=UrlValue(target['url']),
-            label=SelectValue("作業")
-        ),
-        children=Children(*children_list),
-        icon=Emoji("🐶"),
-        cover=FileValue(cover_file_url)
-    )
-
-def material_in_notion_template(db: Database, target):
-    complete_emoji = "✅" if target['已完成'] else "❎"
-    return BaseObject(
-        parent = Parent(db),
-        properties = Properties(
-            Title = TitleValue(target['title']),
-            Course = SelectValue(target['course']),
-            ID = TextValue(target['ID']),
-            # Deadline = DateValue(NotionDate(**target['deadline'])),
-            link = UrlValue(target['url']),
-            label = SelectValue("教材")
-        ),
-        children = Children(
-            # CallOutBlock(f"發佈人 {target['發佈者']}  觀看數 {target['觀看數']}  教材類型 {target['subtype']}", color=Colors.Background.green),
-            CallOutBlock(f"教材類型 {target['subtype']}", color=Colors.Background.green),
-            CallOutBlock(f"完成條件: {target['完成條件']}  進度: {target['完成度']}  已完成: " + complete_emoji, color=Colors.Background.red),
-
-        )
-    )
-
-# def get_config():
-#     load_dotenv()
-#     auth = os.getenv("NOTION_AUTH")
-#     notion_bot = Notion(auth)
-#     db: Database = notion_bot.search("CONFIG")
-#     db_table = db.query_database_dataframe()
-#     table = {
-#          k: v for k, v in zip(db_table['KEY'], db_table['VALUE'])
-#     }
-#     return {
-#         "DATABASE_NAME": table['DATABASE_NAME'],
-#         "ACCOUNT": table['STUDENT_ID'],
-#         "PASSWORD": table['PASSWORD'],
-#     }
-
-# just for testing
-
-def get_id_col(db_col: List[Dict]) -> List[str]:
-    return [p['properties']['ID']['rich_text'][0]['plain_text'] for p in db_col]
-
-
-async def update_all_homework_info_to_notion_db(homeworks: List[Dict], db: Database):
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-        object_index = get_id_col(db.query())
-        tasks = []
-        for r in homeworks:
-            if r['ID'] not in object_index:
-                print(f"upload homework : {r['title']} to homework database")
-                tasks.append(db.async_post(homework_in_notion_template(db, r), session))
-        await asyncio.gather(*tasks)
-
-
-async def update_all_bulletin_info_to_notion_db(bulletins: List[Dict], db: Database):
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-        object_index = get_id_col(db.query())
-        tasks = []
-        for r in bulletins:
-            if r['ID'] not in object_index:
-                print(f"upload bulletin : {r['title']} to bulletin database")
-                tasks.append(db.async_post(builtin_in_notion_template(db, r), session))
-        await asyncio.gather(*tasks)
-
-async def update_all_material_info_to_notion_db(materials: List[Dict], db: Database):
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-        object_index = get_id_col(db.query())
-        tasks = []
-        for r in materials:
-            if r != None and r['ID'] not in object_index:
-                print(f"upload material : {r['title']} to material database")
-                tasks.append(db.async_post(material_in_notion_template(db, r), session))
-        await asyncio.gather(*tasks)
--- eeclass_line_bot/views.py
+++ eeclass_line_bot/views.py
@@ -1,4 +1,3 @@
-import requests
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,27 +8,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookParser, WebhookHandler
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage, FollowEvent, TemplateSendMessage, ButtonsTemplate, \
-    MessageAction, PostbackAction, PostbackEvent
+from linebot.models import MessageEvent, TextSendMessage
 from concurrent.futures import ThreadPoolExecutor
 from .chatBotExtension import handle
 from .chatBotModel import *
-
-# The URL of this server
-
-
-# def check_user_info(user: LineUser) -> str:
-#     is_connect_with_notion = user.notion_token is not None
-#     print(user.eeclass_username, user.eeclass_password)
-#     is_connect_with_eeclass = user.eeclass_password is not None and user.eeclass_username is not None
-#     return f"""確認身份訊息\n \
-# Notion 資料庫連線 {is_connect_with_notion}\n \
-# EECLASS 連線 {is_connect_with_eeclass}\n \
-# 如需連線 Notion 資料庫請打 notion 或 重新授權Notion\n \
-# 如需連線 EECLASS 請說 eeclass\n \
-#     """
-    # self.line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
-
 
 class LineBotCallbackView(View):
     line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
@@ -57,11 +39,6 @@ class LineBotCallbackView(View):
         for event in events:
             if isinstance(event, MessageEvent):
                 self.message_handler(event)
-                # m_text = event.message.text
-                # message = [TextSendMessage(text=m_text)]
-                # line_bot_api.reply_message(event.reply_token, message)
-            if isinstance(event, PostbackEvent):
-                self.handle_postback(event)
 
         return HttpResponse()
 
@@ -75,76 +52,6 @@ class LineBotCallbackView(View):
             if len(replies):
                 self.line_bot_api.reply_message(event.reply_token, replies[-1])
         self.threadPoolExecutor.submit(reply, event)
-        # print("User ID:", event.source.user_id)  # 打印出 User ID
-        # text: str = event.message.text
-        # user_id = event.source.user_id
-        # user_exists = LineUser.objects.filter(line_user_id=user_id)
-        # if not user_exists:
-        #     # 建立新的user資料
-        #     print('建立新的資料')
-        #     user = LineUser(line_user_id=user_id)
-        #     user.save()
-        # else:
-        #     user = LineUser.objects.get(line_user_id=user_id)
-
-        # if text == "重新授權Notion":
-        #     state = str(uuid.uuid4())
-        #     cache.set(state, user_id, timeout=300)
-        #     u = f"https://www.notion.so/install-integration?response_type=code&client_id=5f8acc7a-6c3a-4344-b9e7-3c63a8fad01d&redirect_uri=https%3A%2F%2Fquan.squidspirit.com%2Fnotion%2Fredirect%2F&owner=user&state={state}"
-        #     message = f"請透過連結登入 {u}"
-        #     self.line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
-
-        # elif text.lower() == "notion":
-        #     user_notion_token = LineUser.objects.get(line_user_id=user_id).notion_token
-        #     state = str(uuid.uuid4())
-        #     # print(user_notion_token)
-        #     # print(state)
-        #     u = f"https://www.notion.so/install-integration?response_type=code&client_id=5f8acc7a-6c3a-4344-b9e7-3c63a8fad01d&redirect_uri=https%3A%2F%2Fquan.squidspirit.com%2Fnotion%2Fredirect%2F&owner=user&state={state}"
-        #     cache.set(state, user_id, timeout=300)
-        #     # message = f"請透過連結登入 {self.server_url}/notion/auth/{state}" if not user_notion_token else f"使用者已經成功連線至Notion"
-        #     message = f"請透過連結登入 {u}" if not user_notion_token else f"使用者已經成功連線至Notion"
-
-        #     self.line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
-        # elif text == "設定":
-        #     message = check_user_info(user)
-        #     self.line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
-        # elif text.lower() == "eeclass":
-        #     button = TemplateSendMessage(
-        #         alt_text= "eeclass 連線",
-        #         template=ButtonsTemplate(
-        #             title='eeclass 連線設定',
-        #             text='請選擇連線設定',
-        #             actions=[
-        #                 PostbackAction(
-        #                     label='設定帳號',
-        #                     data='action=設定帳號'
-        #                 ),
-        #                 MessageAction(
-        #                     label='設定密碼',
-        #                     text='設定密碼'
-        #                 ),
-        #                 MessageAction(
-        #                     label='連線測試',
-        #                     text='連線測試'
-        #                 )
-        #             ]
-        #         ))
-        #     self.line_bot_api.reply_message(event.reply_token, button)
-
-        #     # message = "輸入 帳號 <eeclass帳號>\n輸入 密碼 <eeclass密碼>"
-        # else:
-        #     print(text)
-        #     self.line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
-
-    # @handler.add(PostbackEvent)
-    # def handle_postback(self, event):
-    #     postback_data = event.postback.data  # This will contain 'action=set_password'
-    #     print(postback_data)
-    #     if postback_data == 'action=設定帳號':
-    #         self.line_bot_api.reply_message(event.reply_token, TextSendMessage(text="輸入EECLASS帳號"))
-     
-    # def connect_with_eeclass(self, message):
-    #     pass
 
     @classmethod
     def notion_auth_callback(cls, user_id):
