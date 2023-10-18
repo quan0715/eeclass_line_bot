20729b5f3641270769aa7c82bd60016349312958
AlbertDoggyLin <albertlin2468@gmail.com>
2023-10-18 22:55:27 +0800

    function

--- eeclass_line_bot/schedulingModel.py
+++ eeclass_line_bot/schedulingModel.py
@@ -10,9 +10,9 @@ def get_scheduling_job(user_id):
         from .views import LineBotCallbackView
         message = TextSendMessage(text='成功獲取課程資料')
         # TODO fix bug of following command
-        # user, created = LineUser.objects.get_or_create(line_user_id=user_id)
-        # coro = fetch_all_eeclass_data(user.eeclass_username,user.eeclass_password, user)
-        # asyncio.run(coro)
-        # print(f'啟動: 目前時間{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        user, created = LineUser.objects.get_or_create(line_user_id=user_id)
+        coro = fetch_all_eeclass_data(user.eeclass_username,user.eeclass_password, user)
+        asyncio.run(coro)
+        print(f'啟動: 目前時間{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
         LineBotCallbackView.push_message(user_id, message)
     return scheduling
